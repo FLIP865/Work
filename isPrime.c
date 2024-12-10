b79ff9ead851463029a8ad411310afdfa083ddf1
FLIP865 <148561573+FLIP865@users.noreply.github.com>
2024-12-10 05:32:03 +0200

    Update isPrime.c

--- isPrime.c
+++ isPrime.c
@@ -39,7 +39,7 @@ int main() {
                 while (getchar() != '\n');
             }
         } else {
-            printf("Please enter leatters! ");
+            printf("Please enter numbers! ");
             while(getchar() != '\n');
         }
     }
