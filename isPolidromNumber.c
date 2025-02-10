3147f7ca2903d8d2c5899d0fcb959884d10dcacc
FLIP865 <148561573+FLIP865@users.noreply.github.com>
2025-02-10 11:38:17 +0200

    Update isPolidromNumber.c

--- isPolidromNumber.c
+++ isPolidromNumber.c
@@ -1,16 +1,20 @@
 #include <endian.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 bool isPolindrome(int x) {
     if (x < 0) return false;
 
-    int original = x, reversed = 0;
+    int original = x;
+    int64_t reversed = 0;
 
     while (x > 0) {
         int digit = x % 10;
         reversed = reversed * 10 + digit;
         x /= 10;
+
+        if (reversed > INT32_MAX) return false;
     }
     return original == reversed;
 }
